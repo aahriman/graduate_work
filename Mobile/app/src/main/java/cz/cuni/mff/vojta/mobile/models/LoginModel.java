b3e2a7bd635cf3b3a7af3f23cc668de6f776359d
Vojtech Sejkora <vojtech.sejkora@seznam.cz>
2015-12-11 03:32:14 +0100

    [Mobile] Add resources and AndoridManifest

---
+++ Mobile/app/src/main/java/cz/cuni/mff/vojta/mobile/models/LoginModel.java
@@ -0,0 +1,7 @@
+package cz.cuni.mff.vojta.mobile.models;
+
+/**
+ * Created by vojta on 11. 12. 2015.
+ */
+public class LoginModel {
+}
--- Mobile/app/src/main/java/cz/cuni/mff/vojta/mobile/utils/IModelListener.java
+++ Mobile/app/src/main/java/cz/cuni/mff/vojta/mobile/utils/IModelListener.java
@@ -5,7 +5,7 @@ import cz.cuni.mff.vojta.mobile.models.PractiseModel;
 /**
  * Created by vojta on 6. 11. 2015.
  */
-public interface IPractiseModelListener {
+public interface IModelListener {
 
     public void onPrepared(PractiseModel p);
 }
