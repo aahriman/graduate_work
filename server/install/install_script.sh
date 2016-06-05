e6ccb11173a8aa276ddf72b701433a7b6d5db8f9
Vojtěch Sejkora <vojtech.sejkora@gmail.com>
2016-06-05 22:28:03 +0200

    [SERVER] To install script was added building database schema

--- server/install/install_script.sh
+++ server/install/install_script.sh
@@ -30,3 +30,15 @@ then
   echo "installation flask"
   pip3 install flask 
 fi
+
+
+echo "write user to database:"
+read user
+echo "write passwd to database:"
+read passwd
+
+files=$(ls ../database/sql/*.sql)
+for i in $files; do
+   echo "execute $i"
+   mysql -p$passwd -u$user < $i
+done
